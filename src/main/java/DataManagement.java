9575c5a9039870c84239e0fca3a3323afc5354b8
NootanVijapure <nootanvijapure@gmail.com>
2024-01-29 14:21:10 +0100

    Create DataManagement.java

---
+++ src/main/java/DataManagement.java
@@ -0,0 +1,19 @@
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DataManagement {
+
+    public static void sqlCreateTable(Statement statement) throws SQLException {
+        dropTable(statement);
+       //sql query to create table
+    }
+
+    public static void insertDataIntoTable(Statement statement) throws SQLException {
+       
+    }
+
+    private static void dropTable(Statement statement) throws SQLException {
+      //drop if exists
+  
+
+}
